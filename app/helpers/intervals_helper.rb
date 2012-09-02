3c05ace30ac2cac5529e41cc3d58b989d01a18af
joscha <zippelman@web.de>
2012-09-02 11:49:34 +0200

    moved duration view helper to application to use it in user views; show/edit user value formatting added

--- app/helpers/intervals_helper.rb
+++ app/helpers/intervals_helper.rb
@@ -16,26 +16,4 @@ module IntervalsHelper
 			''
 		end
 	end
-	def calcuate_hours_mins_secs(duration)
-		absolute_value = duration.abs
-		hours_rest = absolute_value.divmod(3600)
-		mins_rest = hours_rest.last.divmod(60)
-		{:hours => hours_rest.first,:mins => mins_rest.first,:secs =>mins_rest.last,:negative => duration < 0}
-	end
-	def format_duration(hours_mins_secs)
-		sign = hours_mins_secs[:negative] ? '- ' : ''
-		"#{sign}#{hours_mins_secs[:hours]}h #{hours_mins_secs[:mins]}mins #{hours_mins_secs[:secs]}secs"
-	end
-
-	def format_safe_duration(enough_message, duration)
-		if duration
-			if duration <= 0
-				enough_message
-			else
-				format_duration calcuate_hours_mins_secs duration
-			end
-		else
-			''
-		end
-	end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  it '-3601 is 1h 0 mins 1 secs and negative' do
+    result = helper.calcuate_hours_mins_secs -3601
+    result[:negative].should eq true
+    result[:hours].should eq 1
+    result[:mins].should eq 0
+    result[:secs].should eq 1
+  end
+  it '-3601 secs will be displayed as - 1h 0mins 1secs' do
+    helper.format_duration({:hours => 1,:mins => 0,:secs => 1, :negative => true}).should eq '- 1h 0mins 1secs'
+  end
+  it '3601 secs will be displayed as 1h 0mins 1secs' do
+    helper.format_duration({:hours => 1,:mins => 0,:secs => 1}).should eq '1h 0mins 1secs'
+  end
+  it 'nil duration will be displayed as nothing' do
+    helper.format_safe_duration(nil, nil).should eq ''
+  end
+  it '0 duration will be displayed as it is enough' do
+    helper.format_safe_duration('it is enough', 0).should eq 'it is enough'
+  end
+  it '3661 duration will be displayed as 1h 1mins 1secs' do
+    helper.format_safe_duration('it is enough', 3661).should eq '1h 1mins 1secs'
+  end
+end
